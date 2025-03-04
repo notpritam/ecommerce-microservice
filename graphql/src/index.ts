9d2ed9b4e68ce924bbd8f18fcef50c29028302f8
notpritam <notpritamsharma@gmail.com>
2025-03-04 22:34:10 +0530

    Project Setup with Docker and Hot Reload Done

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -1,3 +1,16 @@
 import express from "express";
+import { createServer } from "http";
 
-console.log("Hello, world!");
+const app = express();
+
+const httpServer = createServer(app);
+
+httpServer.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
+
+app.get("/", (req, res) => {
+  res.send("GraphQL Gateway2");
+});
+
+console.log("GraphQL Gateway2");
